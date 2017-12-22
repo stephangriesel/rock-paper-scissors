a0042b41220c96d21d85070e721969fb8075e653
Stephan Griesel <sgriesel@gmail.com>
2017-12-22 06:33:05 +0100

    caching the dom

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-//
+// caching the dom...
+
+const userScore = 0;
+const computerScore = 0;
+const userScore_span = document.getElementById("user-score");
+const computerScore_span = document.getElementById("computer-score");
+const scoreBoard_div = document.querySelector(".score-board");
+const result_div = document.querySelector(".result");
+const rock_id = document.getElementById("r");
+const paper_id = document.getElementById("p");
+const scissors_id = document.getElementById("s");
